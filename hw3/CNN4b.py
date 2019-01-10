8af813edbd31cfe3f0bc7051cf41c2182c4c15cf
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-01-10 18:52:43 +0100

    HW3 started

---
+++ hw3/CNN4b.py
@@ -0,0 +1,28 @@
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        # conv2d first parameter is the number of kernels at input (you get it from the output value of the previous layer)
+        # conv2d second parameter is the number of kernels you wanna have in your convolution, so it will be the n. of kernels at output.
+        # conv2d third, fourth and fifth parameters are, as you can read, kernel_size, stride and zero padding :)
+        self.conv1 = nn.Conv2d(3, 128, kernel_size=5, stride=2, padding=0)
+        self.conv1_bn = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=0)
+        self.conv2_bn = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=0)
+        self.conv3_bn = nn.BatchNorm2d(128)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv_final = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=0)
+        self.conv_final_bn = nn.BatchNorm2d(256)
+        self.fc1 = nn.Linear(256 * 4 * 4, 8192)
+        self.fc2 = nn.Linear(8192, n_classes)  # last FC for classification
+
+    def forward(self, x):
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = F.relu(self.conv2_bn(self.conv2(x)))
+        x = F.relu(self.conv3_bn(self.conv3(x)))
+        x = F.relu(self.pool(self.conv_final_bn(self.conv_final(x))))
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        # hint: dropout goes here!
+        x = self.fc2(x)
+        return x
---
+++ hw3/CNN5bb.py
@@ -0,0 +1,48 @@
+# function to define the convolutional network
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        # conv2d first parameter is the number of kernels at input (you get it from the output value of the previous layer)
+        # conv2d second parameter is the number of kernels you wanna have in your convolution, so it will be the n. of kernels at output.
+        # conv2d third, fourth and fifth parameters are, as you can read, kernel_size, stride and zero padding :)
+        self.conv1 = nn.Conv2d(3, 128, kernel_size=5, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=0)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv_final = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=0)
+        self.fc1 = nn.Linear(256 * 4 * 4, 4096)
+        self.fc2 = nn.Linear(4096, n_classes)  # last FC for classification
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.pool(self.conv_final(x)))
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        # hint: dropout goes here!
+        #x = F.dropout2d(x, p=0.5)
+        x = self.fc2(x)
+        return x
+
+    ####RUNNING CODE FROM HERE:
+
+
+start = time.time()
+
+# transform are heavily used to do simple and complex transformation and data augmentation
+transform_train = transforms.Compose(
+    [
+        # transforms.RandomHorizontalFlip(),
+        transforms.Resize((40, 40)),
+        transforms.RandomCrop(32),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+transform_test = transforms.Compose(
+    [
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
